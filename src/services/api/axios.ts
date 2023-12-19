ed2e654dace5cbc5fba85f471bb16c6c6d04235a
Eduardo Conti <es.eduardoconti@gmail.com>
2023-12-19 10:29:07 -0300

    fix: ajuste timeout

--- src/services/api/axios.ts
+++ src/services/api/axios.ts
@@ -6,7 +6,7 @@ export function getAPIClient(ctx?: any) {
 
   const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL,
-    timeout: 10000,
+    timeout: 5000,
   });
 
   api.interceptors.request.use((config) => {
